954b3939a06571328843e951a92a5a38243f9e13
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-06-18 16:26:53 -0700

    rebuild

--- AngularFrontend/src/app/app-routing.module.ts
+++ AngularFrontend/src/app/app-routing.module.ts
@@ -1,34 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ForumComponent } from './forum/forum.component';
-import { PostComponent } from './post/post.component';
-import { ArchiveComponent } from './archive/archive.component';
-import { WikiComponent } from './wiki/wiki.component';
-import { IntroductionComponent } from './introduction/introduction.component';
-import { AdminComponent } from "./admin/admin.component";
-
-const routes: Routes = [
-	{path: '', component: HomeComponent},
-	{path: 'login', component: LoginComponent},
-	{path: 'registration', component: RegistrationComponent},
-	{path: 'profile', component: ProfileComponent},
-	{path: 'forum', component: ForumComponent},
-	{path: 'post', component: PostComponent},
-	{path: 'archive', component: ArchiveComponent},
-	{path: 'wiki', component: WikiComponent},
-	{path: 'introduction', component: IntroductionComponent},
-	{path: 'admin', component: AdminComponent},
-	{path: '**', pathMatch: 'full', redirectTo: ''},
-];
+const routes: Routes = [];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, {relativeLinkResolution: 'legacy'})],
-	exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
